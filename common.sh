5b544bf843d5ea53e9670fb750c8d4151e776493
sulaimonkhan <sulaimonkhan04@gmail.com>
2023-11-02 20:52:29 +0530

    shipping

--- common.sh
+++ common.sh
@@ -3,7 +3,33 @@ nocolor="\e[0m"
 log_file="/tmp/roboshop.log"
 app_path="/app"
 
+app_presetup() {
+  echo -e "${color} Add Application User ${nocolor}"
+  useradd roboshop &>>$log_file   
 
+  echo -e "${color}Create Application Directory${nocolor}"
+  rm -rf /app &>>$log_file
+  mkdir /app   
+
+  echo -e "${color} Download Application Content ${nocolor}"
+  curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>$log_file
+
+  echo -e "${color} Extract Application Content ${nocolor}"
+  cd ${app_path}
+  unzip /tmp/$component.zip &>>$log_file
+   
+
+}
+
+systemd_setup(){
+  echo -e "${color} Setup SystemD service ${nocolor}"
+  cp /home/centos/codedry/$component.service /etc/systemd/system/$component.service &>>$log_file
+
+  echo -e "${color} Start $component Service ${nocolor}"
+  systemctl daemon-reload  &>>$log_file
+  systemctl enable $component  &>>$log_file
+  systemctl restart $component  &>>$log_file       
+}
 
 nodejs () {
   echo -e "${color} configuring Nodejs Repos ${nocolor}"
@@ -12,33 +38,14 @@ nodejs () {
   echo -e "${color} Install Nodejs ${nocolor}"
   yum install nodejs -y &>>$log_file
 
-  echo -e "${color} Add Application User ${nocolor}"
-  useradd roboshop &>>$log_file
-
-  echo -e "${color} Create Application Directory ${nocolor}"
-  rm -rf ${app_path} &>>$log_file
-  mkdir ${app_path}
-
-  echo -e "${color} Download Application Content ${nocolor}"
-  curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip &>>$log_file
-  cd ${app_path}
-
-  echo -e "${color} Extract Application Content ${nocolor}"
-  unzip /tmp/$component.zip &>>$log_file
-  cd ${app_path} 
-
+  app_presetup
+  
   echo -e "${color} Install Nodejs Dependencies ${nocolor}"
   npm install &>>$log_file
 
-  echo -e "${color} Setup SystemD service ${nocolor}"
-  cp /home/centos/codedry/$component.service /etc/systemd/system/$component.service &>>$log_file
-
-  echo -e "${color} Start $component Service ${nocolor}"
-  systemctl daemon-reload  &>>$log_file
-  systemctl enable $component  &>>$log_file
-  systemctl restart $component  &>>$log_file    
+  systemd_setup 
 }
-mongo_schema_setup () {
+mongo_schema_setup() {
   echo -e "${color} Copy MongoDB Repo file ${nocolor}"
   cp /home/centos/codedry/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>$log_file
 
@@ -48,4 +55,26 @@ mongo_schema_setup () {
   echo -e "${color} Load Schema ${nocolor}"
   mongo --host mongodb-dev.devopsb72.site <${app_path}/schema/$component.js &>>$log_file
 }
-  
+
+mysql_schema_setup() {
+  echo -e "${color} Install MySQL Client${nocolor}"
+  yum install mysql -y &>>$log_file
+
+  echo -e "${color} Load Schema  ${nocolor}"
+  mysql -h mysql-dev.devopsb72.site -uroot -pRoboShop@1 < /app/schema/${component}.sql &>>$log_file    
+}
+
+maven() {
+  echo -e "${color} Install Maven ${nocolor}"
+  yum install maven -y &>>$log_file
+
+ app_presetup
+
+  echo -e "${color} Download Maven Dependencies ${nocolor}"
+  mvn clean package &>>$log_file
+  mv target/${component}-1.0.jar ${component}.jar &>>$log_file
+
+  mysql_schema_setup
+
+  systemd_setup   
+}   
