4e917c5c5ffcdf6aece3261ec5505b997d390995
sulaimonkhan <sulaimonkhan04@gmail.com>
2023-11-07 20:35:35 +0530

    asa

--- common.sh
+++ common.sh
@@ -1,11 +1,11 @@
-color="\e[36m"
+color="\e[35m"
 nocolor="\e[0m"
 log_file="/tmp/roboshop.log"
 app_path="/app"
 user_id=$(id -u)
 if [ $user_id -ne 0 ]; then
-  echo script should be running with sudo
-  exit
+  echo Script should be running with sudo
+  exit 1
 fi
 
 stat_check() {
@@ -14,40 +14,38 @@ stat_check() {
   else
     echo FAILURE
     exit 1
-  fi    
+  fi
 }
 
-
 app_presetup() {
-  echo -e "${color} Add Application User ${nocolor}"
+  echo -e "${color} Add Application User${nocolor}"
   id roboshop &>>$log_file
   if [ $? -eq 1 ]; then
-    useradd roboshop &>>$log_file   
-  fi  
+    useradd roboshop  &>>$log_file
+  fi
   stat_check $?
 
-  echo -e "${color}Create Application Directory${nocolor}"
-  rm -rf /app &>>$log_file
-  mkdir /app   
+  echo -e "${color} Create Application Directory ${nocolor}"
+  rm -rf /app   &>>$log_file
+  mkdir /app
   stat_check $?
-  
+
   echo -e "${color} Download Application Content ${nocolor}"
-  curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>$log_file
+  curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip  &>>$log_file
   stat_check $?
 
-  echo -e "${color} Extract Application Content ${nocolor}"
+  echo -e "${color} Extract Application Content${nocolor}"
   cd ${app_path}
-  unzip /tmp/$component.zip &>>$log_file
+  unzip /tmp/$component.zip  &>>$log_file
   stat_check $?
-
 }
 
 systemd_setup() {
-  echo -e "${color} Setup SystemD service ${nocolor}"
-  cp /home/centos/codedry/$component.service /etc/systemd/system/$component.service &>>$log_file
-  sed -i -e "s/roboshop_app_password/$roboshop_app_password/" /etc/systemd/system/$component.service
-  stat_check $? 
-  
+  echo -e "${color} Setup SystemD Service  ${nocolor}"
+  cp /home/centos/codedry/$component.service /etc/systemd/system/$component.service  &>>$log_file
+  sed -i -e "s/roboshop_app_password/$roboshop_app_password/"  /etc/systemd/system/$component.service
+  stat_check $?
+
   echo -e "${color} Start $component Service ${nocolor}"
   systemctl daemon-reload  &>>$log_file
   systemctl enable $component  &>>$log_file
@@ -55,29 +53,31 @@ systemd_setup() {
   stat_check $?
 }
 
-nodejs () {
-  echo -e "${color} configuring Nodejs Repos ${nocolor}"
-  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$log_file
+nodejs() {
+  echo -e "${color} Configuring NodeJS Repos ${nocolor}"
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash  &>>$log_file
   stat_check $?
 
-  echo -e "${color} Install Nodejs ${nocolor}"
-  yum install nodejs -y &>>$log_file
+  echo -e "${color} Install NodeJS${nocolor}"
+  yum install nodejs -y  &>>$log_file
   stat_check $?
 
   app_presetup
-  
-  echo -e "${color} Install Nodejs Dependencies ${nocolor}"
-  npm install &>>$log_file
+
+  echo -e "${color} Install NodeJS Dependencies${nocolor}"
+  npm install  &>>$log_file
   stat_check $?
-  systemd_setup 
+
+  systemd_setup
 }
+
 mongo_schema_setup() {
   echo -e "${color} Copy MongoDB Repo file ${nocolor}"
-  cp /home/centos/codedry/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>$log_file
+  cp /home/centos/codedry/mongodb.repo /etc/yum.repos.d/mongodb.repo  &>>$log_file
   stat_check $?
 
   echo -e "${color} Install MongoDB Client ${nocolor}"
-  yum install mongodb-org-shell -y &>>$log_file
+  yum install mongodb-org-shell -y  &>>$log_file
   stat_check $?
 
   echo -e "${color} Load Schema ${nocolor}"
@@ -86,8 +86,8 @@ mongo_schema_setup() {
 }
 
 mysql_schema_setup() {
-  echo -e "${color} Install MySQL Client${nocolor}"
-  yum install mysql -y &>>$log_file
+  echo -e "${color} Install MySQL Client ${nocolor}"
+  yum install mysql -y  &>>$log_file
   stat_check $?
 
   echo -e "${color} Load Schema ${nocolor}"
@@ -97,33 +97,32 @@ mysql_schema_setup() {
 
 maven() {
   echo -e "${color} Install Maven ${nocolor}"
-  yum install maven -y &>>$log_file
+  yum install maven -y  &>>$log_file
   stat_check $?
 
   app_presetup
-
+  
   echo -e "${color} Download Maven Dependencies ${nocolor}"
-  mvn clean package &>>$log_file
-  mv target/${component}-1.0.jar ${component}.jar &>>$log_file
+  mvn clean package  &>>$log_file
+  mv target/${component}-1.0.jar ${component}.jar  &>>$log_file
   stat_check $?
-
+  
   mysql_schema_setup
+  systemd_setup
 
-  systemd_setup   
-}   
+}
 
-python () {
+python() {
   echo -e "${color} Install Python ${nocolor}"
-  yum install python36 gcc python3-devel -y &>>$log_file
+  yum install python36 gcc python3-devel -y &>>/tmp/roboshop.log
   stat_check $?
 
   app_presetup
 
   echo -e "${color} Install Application Dependencies ${nocolor}"
-  cd /app 
-  pip3.6 install -r requirements.txt &>>$log_file
-  stat_check $?   
+  cd /app
+  pip3.6 install -r requirements.txt &>>/tmp/roboshop.log
+  stat_check $?
 
-  
-  systemd_setup 
+  systemd_setup
 }
